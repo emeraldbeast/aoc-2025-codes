1eb128b56f3b63d9022dd077e8745d164ca07d89
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2025-12-03 17:20:45 +0530

    Added Question 3

--- Day3/partA.py
+++ Day3/partA.py
@@ -1,1 +1,19 @@
-#Code coming tmrw
+try:
+    ans = 0
+    while True:
+        k = 2 # You can change this to k = 12 to get answer for first part
+        s = input()
+        n = len(s)
+        L = []
+        remove = n-k 
+        for ch in s:
+            d = int(ch)
+            while L and remove>0 and int(L[-1])<d:
+                L.pop()
+                remove -= 1
+            L.append(str(d))
+        L = L[:k]
+        res = "".join(L)
+        ans+=int(res)
+except EOFError:
+    print(ans)
