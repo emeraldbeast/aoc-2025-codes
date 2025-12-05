ab3ce5c0e0e41f9a14a4e3113e59d97005634be7
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2025-12-05 10:50:25 +0530

    Completed Day5

--- Day5/partA.py
+++ Day5/partA.py
@@ -1,1 +1,21 @@
-#Adding soon
+try:
+    ans = 0
+    intervals = []
+    while True:
+        s = input()
+        if s=="":
+            intervals.sort()
+            continue 
+        if "-" in s:
+            L = s.split("-") 
+            l,r = int(L[0]),int(L[1])
+            intervals.append((l,r))   
+            continue 
+        check = int(s)
+        for x in intervals:
+            l,r = x[0],x[1] 
+            if l<=check<=r:
+                ans+=1   
+                break            
+except:
+    print(ans)
--- Day5/partB.py
+++ Day5/partB.py
@@ -1,1 +1,26 @@
-#Adding soon
+try:
+    mx = 0
+    intervals = []
+    while True:
+        s = input()
+        if s=="":
+            intervals.sort()
+            continue
+        if "-" in s:
+            L = s.split("-")
+            l,r = int(L[0]),int(L[1])
+            mx = max(mx,r)
+            intervals.append((l,r))
+            continue
+        merged = []
+        for l,r in intervals:
+            if not merged or merged[-1][1]<l-1:
+                merged.append([l,r])
+            else:
+                merged[-1][1] = max(merged[-1][1],r)
+
+except:
+    ans = 0
+    for l,r in merged:
+        ans+=(r-l+1)
+    print(ans)
