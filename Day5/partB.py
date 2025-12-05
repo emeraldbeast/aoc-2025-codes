d95a4a41c6501dea04e9de42fb74b7138b195dbb
Dhananjay <f20240506@goa.bits-pilani.ac.in>
2025-12-05 12:21:53 +0530

    Made some changes, now more optimal

--- Day5/partB.py
+++ Day5/partB.py
@@ -10,15 +10,14 @@ try:
             l,r = int(L[0]),int(L[1])
             intervals.append((l,r))
             continue
-        merged = []
-        for l,r in intervals:
-            if not merged or merged[-1][1]<l-1:
-                merged.append([l,r])
-            else:
-                merged[-1][1] = max(merged[-1][1],r)
-
 except:
+    merged = []
+    for l,r in intervals:
+        if not merged or merged[-1][1]<l-1:
+            merged.append([l,r])
+        else:
+            merged[-1][1] = max(merged[-1][1],r)
     ans = 0
     for l,r in merged:
-        ans+=(r-l+1)
+        ans+=r-l+1
     print(ans)
